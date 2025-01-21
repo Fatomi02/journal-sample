ffbf6d21493f2649d152bed06adaeb1ce2300f64
Fatomi02 <fatomiabdulrahmon@gmail.com>
2025-01-21 00:18:18 +0000

    done

--- src/pages/issue-details/issue-details.jsx
+++ src/pages/issue-details/issue-details.jsx
@@ -13,6 +13,7 @@ export default function IssueDetails() {
     const { volume, issue } = useParams();
       const [isLoading, setIsLoading] = useState(false)
     const [filteredJournals, setFilteredJournals] = useState([]);
+    const [singleJournalDate, setSingleJournalDate] = useState();
     
     useEffect(() => {
         setIsLoading(true)
@@ -22,6 +23,7 @@ export default function IssueDetails() {
                 const filtered = response.data.journals.filter(
                     (journal) => journal.volume === volume && journal.issue === parseInt(issue)
                 );
+                setSingleJournalDate(filtered[0]?.date)
                 setFilteredJournals(filtered);
             }
         }).catch((error) => {
@@ -35,8 +37,10 @@ export default function IssueDetails() {
     return (
         <>
             <Navbar />
-            <div className="w-full mt-[240px] md:mt-[250px] text-center relative z-0 mx-auto bg-[#f6f6f6] py-[30px] shadow-[0_1px_0px_0px_rgba(0,0,0,0.3)]">
-                <h2 className="text-[30px] leading-9 italic text-[#444444]">Volume {volume} Issue {issue} </h2>
+            <div className="w-full mt-[220px] md:mt-[250px] text-center relative z-0 mx-auto bg-[#f6f6f6] py-[30px] shadow-[0_1px_0px_0px_rgba(0,0,0,0.3)]">
+                <h2 className="text-[24px] leading-9 italic text-[#444444]">Volume {volume} Issue {issue}  ({new Date(singleJournalDate).toLocaleDateString('en-US', {
+                                    year: "numeric"
+                                })})</h2>
                 <ul className="flex justify-center gap-4 text-[#797979] mt-3">
                     <li className="hover:underline"><Link to="/">Home</Link></li>
                     <li><span> &gt;</span></li>
@@ -46,12 +50,11 @@ export default function IssueDetails() {
             </div>
             <section className="2xl:w-[64%] bg-[#FCFCFC] mx-auto md:flex justify-between w-[86%] mt-[70px] mb-10">
                 <div className="w-full mx-auto md:w-[70%] text-justify text-[#626262] text-[14px] mt-[-36px] flex flex-col gap-8 leading-7">
-                    <h6 className="text-center font-semibold text-[#d80c6c] text-[20px]">Volume {volume} Issue {issue} </h6>
-                    <ul className="list-disc flex flex-col gap-1">
+                    <ul className="list-disc flex flex-col gap-1 pl-[30px] lg:pl-0">
                         {isLoading && <div className="flex justify-center items-center h-full w-full"><span className="loader"></span></div> }
                         {(!isLoading && filteredJournals.length > 0) &&
                         filteredJournals.map((journal) => (
-                            <li key={journal._id} className="font-medium text-[#d80c6c]">
+                            <li key={journal._id} className="font-medium text-[14px] tracking-tighter text-[#d80c6c]">
                                 <Link to={`/journal/${volume}/${journal.issue}/${journal.page}}`} className="text-[16px] hover:underline">{journal.title}</Link>
                             </li>
                         ))
